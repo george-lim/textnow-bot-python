7f7fa4d0d8625df36e9dd31925fa95badd8c313c
rohanmodil123 <61063602+rohanmodil123@users.noreply.github.com>
2021-02-01 16:26:00 -0500

    Update textnow_bot.py

--- textnow_bot.py
+++ textnow_bot.py
@@ -60,7 +60,6 @@ class AsyncMeta(type):
         return obb
 
 
-
 class AsyncTextNowBot(metaclass=AsyncMeta):
     async def __init__(self, page, cookies=None, username=None, password=None):
         self.page = page
